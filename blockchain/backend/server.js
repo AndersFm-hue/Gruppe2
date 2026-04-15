53a45feaaec7ce75bcda660d2633cee7d8d68e32
AndersFm-hue <andersmathiassen21@outlook.com>
2026-04-15 08:57:54 +0200

    opd

--- blockchain/backend/server.js
+++ blockchain/backend/server.js
@@ -3,19 +3,22 @@ import { pool } from '../db/connect.js';
 
 const db = pool();
 
-const port = 3002;
+const port = 3003;
 const server = express();
 server.use(express.static('frontend'));
 server.use(onEachRequest);
-server.get('/api/artist/:id', onGetArtistById);
+server.get('/api/adressesBycurrency:id',onGetAdressesByCurrency);
 
 
-async function onGetArtistById(request, response) {
+async function onGetAdressesByCurrency(request, response) {
     const id = request.params.id;
     const dbResult = await db.query(`
-        select stage_name, nationality
-        from   artists
-        where  artist_id = $1`, [id]);
+        select address_name
+        from   addresses
+        join transfers using (sender_address_id)
+        join transfers using (receiver_address_id)
+        join currency using (currency_id) 
+        where  currency_symbol = $1`, [id]); // Den skal væk 
     const rows = dbResult.rows;
     if (rows.length === 0) {
         response.sendStatus(404);
